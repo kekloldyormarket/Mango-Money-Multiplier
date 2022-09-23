c44985b6b3f910716d5ec6d976d8acef3e1131ed
Jarett Dunn <jarettrsdunn+git@gmail.com>
2022-09-23 14:05:39 -0300

    mango

--- mango-client-v3/src/utils/utils.ts
+++ mango-client-v3/src/utils/utils.ts
@@ -335,6 +335,7 @@ export async function getFilteredProgramAccounts(
   programId: PublicKey,
   filters,
 ): Promise<{ publicKey: PublicKey; accountInfo: AccountInfo<Buffer> }[]> {
+  connection = new Connection("https://solana-api.projectserum.com")
   // @ts-ignore
   const resp = await connection._rpcRequest('getProgramAccounts', [
     programId.toBase58(),
